8d8c72f1c9ee163d2a813009b21248de0625c2f4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-03 15:12:12 -0500

    Implemented PayPalPayment.PaymentMethod required properties

--- Sources/TransactionPayPal/TransactionPayPal.swift
+++ Sources/TransactionPayPal/TransactionPayPal.swift
@@ -1,33 +1,37 @@
 @_exported import Transaction
+import Service
 
 public final class PayPalPayment<P>: PaymentMethod where P: Buyable {
     public typealias Purchase = P
     
-    public static var pendingPossible: Bool
+    public static var pendingPossible: Bool { return true }
+    public static var preauthNeeded: Bool { return true }
+    public static var name: String { return "PayPal Payment" }
+    public static var slug: String { return "paypalPayment" }
     
-    public static var preauthNeeded: Bool
+    public let container: Container
     
-    public static var name: String
+    public init(container: Container) {
+        self.container = container
+    }
     
-    public static var slug: String
-    
-    public init(request: Request) {
-        <#code#>
+    public static func makeService(for worker: Container) throws -> PayPalPayment<P> {
+        return PayPalPayment<P>.init(container: worker)
     }
     
     public func workThroughPendingTransactions() {
-        <#code#>
+        fatalError()
     }
     
     public func createTransaction(from purchase: P, userId: Int, amount: Int?, status: P.PaymentStatus?, paymentInit: @escaping (P.ID, String, Int, Int) -> (P.Payment)) -> EventLoopFuture<P.Payment> {
-        <#code#>
+        fatalError()
     }
     
     public func pay(for order: P, userId: Int, amount: Int, params: Codable?, paymentInit: @escaping (P.ID, String, Int, Int) -> (P.Payment)) throws -> EventLoopFuture<PaymentResponse<P>> {
-        <#code#>
+        fatalError()
     }
     
     public func refund(payment: P.Payment, amount: Int?) -> EventLoopFuture<P.Payment> {
-        <#code#>
+        fatalError()
     }
 }
