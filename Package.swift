70b100ce831e021b50df9b2d0c0e5b6edd15d047
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-03 14:50:52 -0500

    Installed PayPal package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "TransactionPayPal", targets: ["TransactionPayPal"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/skelpo/PayPal.git", from: "0.1.0"),
         .package(url: "https://github.com/skelpo/Transaction.git", from: "0.1.3")
     ],
     targets: [
-        .target(name: "TransactionPayPal", dependencies: ["Transaction"]),
+        .target(name: "TransactionPayPal", dependencies: ["Transaction", "PayPal"]),
         .testTarget(name: "TransactionPayPalTests", dependencies: ["TransactionPayPal"]),
     ]
 )
