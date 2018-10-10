6c1911e5ce60292ca28b6a90fd81c321a4f9fee6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-10 10:06:00 -0500

    Made ExecutablePayment.externalID property optional

--- Sources/TransactionPayPal/Protocols.swift
+++ Sources/TransactionPayPal/Protocols.swift
@@ -4,7 +4,7 @@ import PayPal
 public protocol ExecutablePayment {
     var total: String { get }
     var currency: Currency { get }
-    var externalID: String { get }
+    var externalID: String? { get }
 }
 
 public protocol PaymentCreator {
