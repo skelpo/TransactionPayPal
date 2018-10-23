89de04ab44f7e7cf1525526f2b430ef62f293ea9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-23 12:17:47 -0500

    Documented AcceptQueryString struct

--- Sources/TransactionPayPal/Content.swift
+++ Sources/TransactionPayPal/Content.swift
@@ -1,6 +1,11 @@
 import Vapor
 
+/// The query-string values received from PayPal after a transaction is approved.
 public struct AcceptQueryString: Content {
+    
+    /// The ID of the payer for the transaction.
     public let payerID: String
+    
+    /// The ID of the transaction's payment.
     public let paymentID: String
 }
